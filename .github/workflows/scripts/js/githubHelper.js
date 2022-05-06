4cd21fe2893aa300d4fd81498fe69416c21d0a09
Federico Bernal <64086728+FedericoBernal@users.noreply.github.com>
2022-05-06 17:46:51 -0300

    Update githubHelper.js

--- .github/workflows/scripts/js/githubHelper.js
+++ .github/workflows/scripts/js/githubHelper.js
@@ -12,18 +12,9 @@ const checkWorkflowStatus = async (github, context, core, id) => {
   let currentStatus = null;
   let conclusion = null;
   let html_url = null;
-  function wait(ms){
-    var start = new Date().getTime();
-    var end = start;
-    while(end < start + ms) {
-      end = new Date().getTime();
-   }
- }
-  
+  sleep(2000)
+
   console.log('Checking the status for workflow ' + id)
-  function wait(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
   do {
     let workflowLog = await github.rest.actions.listWorkflowRuns({
       owner: context.repo.owner,
@@ -31,7 +22,6 @@ const checkWorkflowStatus = async (github, context, core, id) => {
       workflow_id: id,
       per_page: 1
     })
-    wait(20000)
     if (workflowLog.data.total_count > 0) {
       currentStatus = workflowLog.data.workflow_runs[0].status
       conclusion = workflowLog.data.workflow_runs[0].conclusion
@@ -41,11 +31,20 @@ const checkWorkflowStatus = async (github, context, core, id) => {
       break
     }
     console.log(new Date().toISOString() + ' - status: ' + currentStatus)
+    sleep(20000)
   } while (currentStatus != 'completed');
 
   if (conclusion != 'success') {
     core.setFailed('Workflow execution failed. For more details go to ' + html_url)
   }
+}
+
+const sleep = (milliseconds) => {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
 }
 
 module.exports = {

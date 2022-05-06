a58c2e505d026054613fb3028496ce3a448250a5
Federico Bernal <64086728+FedericoBernal@users.noreply.github.com>
2022-05-06 11:18:54 -0300

    update sleep

--- .github/workflows/scripts/js/createCommitStatuses.js
+++ .github/workflows/scripts/js/createCommitStatuses.js
@@ -1,19 +1,49 @@
-module.exports = async ({ github, context, solutionNamesString }) => {
-    console.log(solutionNamesString)
-
-    let solutionNamesArray = solutionNamesString.split(",")
-
-    await solutionNamesArray.forEach(createCommitStatus)
-
-    async function createCommitStatus(solutionName) {
-        let contextToUse = "build-deploy-" + solutionName
-
-        await github.rest.repos.createCommitStatus({
-            owner: context.repo.owner,
-            repo: context.repo.repo,
-            sha: context.payload.pull_request.head.sha,
-            context: contextToUse,
-            state: "pending"
-        })
+const dispatchWorkflow = async (github, context, id, reference, parameters) => {
+    await github.rest.actions.createWorkflowDispatch({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      workflow_id: id,
+      ref: reference,
+      inputs: parameters
+    })
+  }
+  
+  const checkWorkflowStatus = async (github, context, core, id) => {
+    let currentStatus = null;
+    let conclusion = null;
+    let html_url = null;
+    setTimeout(function(){ 
+      console.log("Waiting") 
+    }, 2000)
+  
+    console.log('Checking the status for workflow ' + id)
+    do {
+      let workflowLog = await github.rest.actions.listWorkflowRuns({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        workflow_id: id,
+        per_page: 1
+      })
+      if (workflowLog.data.total_count > 0) {
+        currentStatus = workflowLog.data.workflow_runs[0].status
+        conclusion = workflowLog.data.workflow_runs[0].conclusion
+        html_url = workflowLog.data.workflow_runs[0].html_url
+      }
+      else {
+        break
+      }
+      console.log(new Date().toISOString() + ' - status: ' + currentStatus)
+      setTimeout(function(){ 
+        console.log("Waiting") 
+      }, 20000)
+    } while (currentStatus != 'completed');
+  
+    if (conclusion != 'success') {
+      core.setFailed('Workflow execution failed. For more details go to ' + html_url)
     }
-}
+  }
+  
+  module.exports = {
+    dispatchWorkflow,
+    checkWorkflowStatus
+  }
---
+++ .github/workflows/scripts/js/githubHelper.js
@@ -0,0 +1,49 @@
+const dispatchWorkflow = async (github, context, id, reference, parameters) => {
+  await github.rest.actions.createWorkflowDispatch({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    workflow_id: id,
+    ref: reference,
+    inputs: parameters
+  })
+}
+
+const checkWorkflowStatus = async (github, context, core, id) => {
+  let currentStatus = null;
+  let conclusion = null;
+  let html_url = null;
+  setTimeout(function(){ 
+    console.log("Waiting") 
+  }, 2000)
+
+  console.log('Checking the status for workflow ' + id)
+  do {
+    let workflowLog = await github.rest.actions.listWorkflowRuns({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      workflow_id: id,
+      per_page: 1
+    })
+    if (workflowLog.data.total_count > 0) {
+      currentStatus = workflowLog.data.workflow_runs[0].status
+      conclusion = workflowLog.data.workflow_runs[0].conclusion
+      html_url = workflowLog.data.workflow_runs[0].html_url
+    }
+    else {
+      break
+    }
+    console.log(new Date().toISOString() + ' - status: ' + currentStatus)
+    setTimeout(function(){ 
+      console.log("Waiting") 
+    }, 20000)
+  } while (currentStatus != 'completed');
+
+  if (conclusion != 'success') {
+    core.setFailed('Workflow execution failed. For more details go to ' + html_url)
+  }
+}
+
+module.exports = {
+  dispatchWorkflow,
+  checkWorkflowStatus
+}
